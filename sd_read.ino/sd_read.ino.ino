39bfb19fa27d7794145c69fbc4698e771ad8b7df
rossebv <rossebv@berkeley.edu>
2017-05-25 22:35:23 -0700

    SD Class

---
+++ sd_read.ino/sd_read.ino.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+#include <SD.h>
+ 
+File logfile;  
+
+#define chipSelect 10
+#define ledPin 3 
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  pinMode(10, OUTPUT);
+  
+  // SD Card Init
+  if (!SD.begin(chipSelect)) {
+    Serial.println(F("SD Card init. failed!"));
+    while(1);
+  } else {
+  Serial.println(F("SD Card init successful!"));
+  }
+  
+  // File Init
+  char filename[15];
+  sprintf(filename, "/LOG00.txt");           //manually input file name
+//  for (uint8_t i = 0; i < 100; i++) {      //uncomment to load first file
+//    filename[4] = '0' + i/10;
+//    filename[5] = '0' + i%10;
+//    if (! SD.exists(filename)) {       
+//      break;
+//    }
+//  }
+  
+  Serial.print(F("reading file: "));
+  Serial.println(filename);
+  logfile = SD.open(filename, FILE_READ);
+  if( ! logfile ) {
+    Serial.print(F("Couldnt create ")); Serial.println(filename);
+    while(1);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
